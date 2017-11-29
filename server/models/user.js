ef6f928573a67dbd718b03117d1f31136127d8e7
adrianadamiec <adrianadamiec@gmail.com>
2017-11-29 20:29:40 +0100

    update config

--- server/models/user.js
+++ server/models/user.js
@@ -45,7 +45,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
     let user = this;
     let access = 'auth';
-    let token = jwt.sign({_id: user._id.toHexString(), access}, 'asd123').toString();
+    let token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
     user.tokens.push({access, token});
     return user.save().then(() => {
@@ -60,7 +60,7 @@ UserSchema.statics.findByToken = function (token) {
 
     try {
 
-        decoded = jwt.verify(token, 'asd123');
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     } catch (e) {
 
